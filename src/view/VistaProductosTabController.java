f83d9dca84db1e25c24ea9355eb779bd99700f78
System32 <System32@DESKTOP-VDEMTT8>
2018-02-07 20:26:23 +0100

    07/02/2018 doble click fila tabla cambia de tab

--- src/view/VistaProductosTabController.java
+++ src/view/VistaProductosTabController.java
@@ -8,6 +8,7 @@ package view;
 import controller.Inventario;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
@@ -17,6 +18,8 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.DialogPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import model.Producto;
 
 /**
@@ -91,6 +94,20 @@ public class VistaProductosTabController implements Initializable {
             // activo los tabs de VistaTabsControlador al seleccionar un elemento de la tabla y envio el objecto seleccionado
             tabsControllerr.activarTabs();
             this.filaSeleccionada = (Producto) newValue;
+
+        });
+
+        // doble click en la fila cambia de tab
+        tablaProductos.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
+                    if (mouseEvent.getClickCount() == 2) {
+                        tabsControllerr.setFilaInformacion(filaSeleccionada);
+                        System.out.println("Double clicked");
+                    }
+                }
+            }
         });
 
         detalles.setOnMouseClicked(e
@@ -98,46 +115,31 @@ public class VistaProductosTabController implements Initializable {
             // cambio de tab al hacer click en ir a detalles boton, llamando al controller de los tabs general (VistaTabsController)
             // envio el objeto con los datos a VistaTabsControler para despues reenviarlo a VistaInformacionTabController
             tabsControllerr.setFilaInformacion(this.filaSeleccionada);
-            
 
         });
 
         borrar.setOnMouseClicked(e
                 -> {
-            String borrarString = " > Código: " + filaSeleccionada.getCodigo() + "\n > Nombre: " + filaSeleccionada.getNombre() + "\n > Stock: " + filaSeleccionada.getStock() + " uds." + 
-                    "\n > Precio: " + filaSeleccionada.getPrecio() + " €" + "\n > Fecha Alta: " + filaSeleccionada.getFechaAlta();
+            String borrarString = " > Código: " + filaSeleccionada.getCodigo() + "\n > Nombre: " + filaSeleccionada.getNombre() + "\n > Stock: " + filaSeleccionada.getStock() + " uds."
+                    + "\n > Precio: " + filaSeleccionada.getPrecio() + " €" + "\n > Fecha Alta: " + filaSeleccionada.getFechaAlta();
             System.out.println(tablaProductos.getItems().size());
-            
+
             Alert alert;
 
             alert = new Alert(AlertType.WARNING, "Contenido de la fila a borrar:\n\n" + borrarString + "\n\nBorrar definitivamente?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
             alert.setHeaderText("Confirmación de borrado");
-            
+
             //css dialog pane
             DialogPane dialogAlert = alert.getDialogPane();
             dialogAlert.getStylesheets().add(getClass().getResource("../css/modena_dark.css").toExternalForm());
             alert.showAndWait();
 
-            
             if (alert.getResult() == ButtonType.YES) {
                 tablaProductos.getItems().remove(tablaProductos.getSelectionModel().getSelectedIndex());
             }
 
-        }
-        );
+        });
 
-//        de.setOnMouseClicked(new EventHandler<MouseEvent>() {
-//            @Override
-//            public void handle(MouseEvent mouseEvent) {
-//                // doble click cambia de tab a Informacion detallada del producto seleccionado
-//                if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
-//                    if (mouseEvent.getClickCount() == 2) {
-//                        tabsControllerr.setTabInformacion();
-//                        System.out.println("Double clicked");
-//                    }
-//                }
-//            }
-//        });
     }
 
     //Es llamado por la apliación principal para tener una referencia de vuelta de si mismo
