47e2405c3a3484f9f307c12476db01f38152067a
Victor <ditochoza@gmail.com>
2018-02-19 00:33:04 +0100

    Botón editar completamente funcional. ComboBox creado con un máximo de 100 en VistaInformacionTab. Control de errores añadido al generador de códigos de barras del VistaInformacionTab. Varias correcciones de errores.

--- src/view/VistaInformacionTabController.java
+++ src/view/VistaInformacionTabController.java
@@ -5,8 +5,9 @@
  */
 package view;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
-import java.io.File;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -19,7 +20,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -36,11 +36,13 @@ public class VistaInformacionTabController implements Initializable {
     private Producto filaSeleccionadaProducto;
     private VistaTabsController tabsController;
 
+    // datos antes de la edición
     private String nombreOld;
     private String precioOld;
     private String descripcionOld;
     private String categoriaOld;
     private String stockOld;
+    private String rutaOld;
 
     // datos
     @FXML
@@ -71,6 +73,12 @@ public class VistaInformacionTabController implements Initializable {
     Button cancelar;
     @FXML
     Button guardar;
+    @FXML
+    JFXButton crear;
+
+    // comboBox
+    @FXML
+    JFXComboBox comboBoxCodigosBarras;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -94,6 +102,14 @@ public class VistaInformacionTabController implements Initializable {
         stockProducto.setText(String.valueOf(filaSeleccionadaProducto.getStock()));
         fechaAltaProducto.setText(String.valueOf(filaSeleccionadaProducto.getFechaAlta()));
         fechaModificacionProducto.setText(String.valueOf(filaSeleccionadaProducto.getFechaModificacion()));
+
+        int numeroCodigosBarras = 100;
+        if (filaSeleccionadaProducto.getStock() < numeroCodigosBarras) {
+            numeroCodigosBarras = filaSeleccionadaProducto.getStock();
+        }
+        for (int i = 0; i < numeroCodigosBarras; i++) {
+            comboBoxCodigosBarras.getItems().add(i + 1);
+        }
     }
 
     private void listeners() {
@@ -104,7 +120,7 @@ public class VistaInformacionTabController implements Initializable {
 
             Alert alert;
 
-            alert = new Alert(Alert.AlertType.WARNING, "Contenido de la fila a borrar:\n\n" + borrarString + "\n\nBorrar definitivamente?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+            alert = new Alert(Alert.AlertType.WARNING, "Contenido de la fila a borrar:\n\n" + borrarString + "\n\n¿Borrar definitivamente?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
             alert.setHeaderText("Confirmación de borrado");
 
             //css dialog pane
@@ -119,101 +135,132 @@ public class VistaInformacionTabController implements Initializable {
         });
 
         editar.setOnMouseClicked(e -> {
-            nombreOld = nombreProducto.getText();
-            precioOld = precioProducto.getText();
-            descripcionOld = descripcionProducto.getText();
-            categoriaOld = categoriaProducto.getText();
-            stockOld = stockProducto.getText();
-            
             modoEditar(true);
         });
 
         cancelar.setOnMouseClicked(e -> {
+            imagenProducto.setImage(new Image(getClass().getResource(rutaOld).toExternalForm()));
             nombreProducto.setText(nombreOld);
             precioProducto.setText(precioOld);
             descripcionProducto.setText(descripcionOld);
             categoriaProducto.setText(categoriaOld);
             stockProducto.setText(stockOld);
-            
+
             modoEditar(false);
         });
 
-        guardar.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent e) {
-                System.out.println("Guardar");
-                String erroresString = "";
+        guardar.setOnMouseClicked((MouseEvent e) -> {
+            System.out.println("Guardar");
+            String erroresString = "";
 
-                // errores nombre producto
-                if (nombreProducto.getText().isEmpty()) {
-                    erroresString += " - El nombre no puede quedar vacío\n";
-                    nombreProducto.setUnFocusColor(Color.RED);
-                } else {
-                    nombreProducto.setUnFocusColor(Color.rgb(42, 46, 55));
-                }
+            // errores nombre producto
+            if (nombreProducto.getText().isEmpty()) {
+                erroresString += " - El nombre no puede quedar vacío\n";
+                nombreProducto.setUnFocusColor(Color.RED);
+            } else {
+                nombreProducto.setUnFocusColor(Color.rgb(42, 46, 55));
+            }
 
-                // errores precio producto
-                if (!precioProducto.getText().isEmpty()) {
-                    try {
-                        Double valor = Double.valueOf(precioProducto.getText());
-                        precioProducto.setUnFocusColor(Color.rgb(42, 46, 55));
-                    } catch (NumberFormatException ex) {
-                        erroresString += " - El precio debe ser un número\n";
-                        precioProducto.setUnFocusColor(Color.RED);
-                    }
-                } else {
-                    erroresString += " - El precio no puede quedar vacío\n";
+            // errores precio producto
+            if (!precioProducto.getText().isEmpty()) {
+                try {
+                    Double valor = Double.valueOf(precioProducto.getText());
+                    precioProducto.setUnFocusColor(Color.rgb(42, 46, 55));
+                } catch (NumberFormatException ex) {
+                    erroresString += " - El precio debe ser un número\n";
                     precioProducto.setUnFocusColor(Color.RED);
                 }
+            } else {
+                erroresString += " - El precio no puede quedar vacío\n";
+                precioProducto.setUnFocusColor(Color.RED);
+            }
 
-                //errores descripcion producto
-                if (descripcionProducto.getText().isEmpty()) {
-                    erroresString += " - La descripción no puede quedar vacía\n";
-                    descripcionProducto.setUnFocusColor(Color.RED);
-                } else {
-                    descripcionProducto.setUnFocusColor(Color.rgb(42, 46, 55));
-                }
+            //errores descripcion producto
+            if (descripcionProducto.getText().isEmpty()) {
+                erroresString += " - La descripción no puede quedar vacía\n";
+                descripcionProducto.setUnFocusColor(Color.RED);
+            } else {
+                descripcionProducto.setUnFocusColor(Color.rgb(42, 46, 55));
+            }
 
-                //errores categoria producto
-                if (categoriaProducto.getText().isEmpty()) {
-                    erroresString += " - La categoría no puede quedar vacía\n";
-                    categoriaProducto.setUnFocusColor(Color.RED);
-                } else {
-                    categoriaProducto.setUnFocusColor(Color.rgb(42, 46, 55));
-                }
+            //errores categoria producto
+            if (categoriaProducto.getText().isEmpty()) {
+                erroresString += " - La categoría no puede quedar vacía\n";
+                categoriaProducto.setUnFocusColor(Color.RED);
+            } else {
+                categoriaProducto.setUnFocusColor(Color.rgb(42, 46, 55));
+            }
 
-                //errores stock producto
-                if (!stockProducto.getText().isEmpty()) {
-                    try {
-                        int valor = Integer.valueOf(stockProducto.getText());
-                        stockProducto.setUnFocusColor(Color.rgb(42, 46, 55));
-                    } catch (NumberFormatException ex) {
-                        erroresString += " - El stock debe ser un número\n";
-                        stockProducto.setUnFocusColor(Color.RED);
-                    }
-                } else {
-                    erroresString += " - El stock no puede quedar vacío\n";
+            //errores stock producto
+            if (!stockProducto.getText().isEmpty()) {
+                try {
+                    int valor = Integer.valueOf(stockProducto.getText());
+                    stockProducto.setUnFocusColor(Color.rgb(42, 46, 55));
+                } catch (NumberFormatException ex) {
+                    erroresString += " - El stock debe ser un número\n";
                     stockProducto.setUnFocusColor(Color.RED);
                 }
+            } else {
+                erroresString += " - El stock no puede quedar vacío\n";
+                stockProducto.setUnFocusColor(Color.RED);
+            }
 
-                if (!erroresString.isEmpty()) {
-                    Alert alert;
-                    alert = new Alert(Alert.AlertType.WARNING, "Se han encontrado los siguientes errores:\n\n" + erroresString, ButtonType.OK);
-                    alert.setHeaderText("Confirmación de borrado");
-                    DialogPane dialogAlert = alert.getDialogPane();
-                    dialogAlert.getStylesheets().add(VistaInformacionTabController.this.getClass().getResource("../css/modena_dark.css").toExternalForm());
-                    alert.showAndWait();
+            if (!erroresString.isEmpty()) {
+                Alert alert;
+                alert = new Alert(Alert.AlertType.WARNING, "Se han encontrado los siguientes errores:\n\n" + erroresString + "\n\nResuelva los errores para poder continuar", ButtonType.OK);
+                alert.setHeaderText("Error");
+                DialogPane dialogAlert = alert.getDialogPane();
+                dialogAlert.getStylesheets().add(VistaInformacionTabController.this.getClass().getResource("../css/modena_dark.css").toExternalForm());
+                alert.showAndWait();
+            } else {
+                System.out.println("Sin errores. Guardando...");
+                filaSeleccionadaProducto.setNombre(nombreProducto.getText());
+                filaSeleccionadaProducto.setPrecio(Double.valueOf(precioProducto.getText()));
+                filaSeleccionadaProducto.setDescripcion(descripcionProducto.getText());
+                filaSeleccionadaProducto.setCategoria(categoriaProducto.getText());
+                filaSeleccionadaProducto.setStock(Integer.valueOf(stockProducto.getText()));
+                filaSeleccionadaProducto.setFechaModificacion(new SimpleDateFormat("dd/MM/yyy HH:mm").format(Calendar.getInstance().getTime()));
+
+                fechaModificacionProducto.setText(String.valueOf(filaSeleccionadaProducto.getFechaModificacion()));
+
+                modoEditar(false);
+                tabsController.actualizarTabla();
+            }
+        });
+
+        crear.setOnMouseClicked((MouseEvent e) -> {
+            String erroresString = "";
+
+            //errores cantidad código de barras
+            if (comboBoxCodigosBarras.getValue() != null) {
+                if (Integer.valueOf(comboBoxCodigosBarras.getValue().toString()) <= filaSeleccionadaProducto.getStock()) {
+                    try {
+                        int valor = Integer.valueOf(comboBoxCodigosBarras.getValue().toString());
+                        comboBoxCodigosBarras.setUnFocusColor(Color.rgb(42, 46, 55));
+                    } catch (NumberFormatException ex) {
+                        erroresString += " - La cantdad debe ser un número\n";
+                        comboBoxCodigosBarras.setUnFocusColor(Color.RED);
+                    }
                 } else {
-                    System.out.println("Sin errores. Guardando...");
-                    filaSeleccionadaProducto.setNombre(nombreProducto.getText());
-                    filaSeleccionadaProducto.setPrecio(Double.valueOf(precioProducto.getText()));
-                    filaSeleccionadaProducto.setDescripcion(descripcionProducto.getText());
-                    filaSeleccionadaProducto.setCategoria(categoriaProducto.getText());
-                    filaSeleccionadaProducto.setStock(Integer.valueOf(stockProducto.getText()));
-                    filaSeleccionadaProducto.setFechaModificacion(new SimpleDateFormat("dd/MM/yyy HH:mm").format(Calendar.getInstance().getTime()));
-
-                    tabsController.actualizarTabla();
+                    erroresString += " - La cantidad no puede ser mayor que el stock\n";
+                    comboBoxCodigosBarras.setUnFocusColor(Color.RED);
                 }
+            } else {
+                erroresString += " - La cantidad no puede quedar vacía\n";
+                comboBoxCodigosBarras.setUnFocusColor(Color.RED);
+            }
+
+            if (!erroresString.isEmpty()) {
+                Alert alert;
+                alert = new Alert(Alert.AlertType.WARNING, "Se han encontrado los siguientes errores:\n\n" + erroresString + "\n\nResuelva los errores para poder continuar", ButtonType.OK);
+                alert.setHeaderText("Error");
+                DialogPane dialogAlert = alert.getDialogPane();
+                dialogAlert.getStylesheets().add(VistaInformacionTabController.this.getClass().getResource("../css/modena_dark.css").toExternalForm());
+                alert.showAndWait();
+            } else {
+                /*
+                Creación de códigos de barras
+                 */
             }
         });
     }
@@ -224,6 +271,14 @@ public class VistaInformacionTabController implements Initializable {
 
     public void modoEditar(boolean mode) {
         if (mode) {
+            // se guardan los datos anteriores a la edición
+            rutaOld = filaSeleccionadaProducto.getRutaFoto();
+            nombreOld = nombreProducto.getText();
+            precioOld = precioProducto.getText();
+            descripcionOld = descripcionProducto.getText();
+            categoriaOld = categoriaProducto.getText();
+            stockOld = stockProducto.getText();
+            
             nombreProducto.setFocusColor(Color.rgb(230, 230, 0));
             nombreProducto.setUnFocusColor(Color.rgb(42, 46, 55));
             precioProducto.setFocusColor(Color.rgb(230, 230, 0));
