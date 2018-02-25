8c402fccde0f14697919b1021c3df6f2686e280e
Cicu <Cicu@DESKTOP-OO8K6L2>
2018-02-25 21:31:46 +0100

    Añado la funcionalidad de convertir la grafica en pdf

--- src/view/VistaEstadisticasTabController.java
+++ src/view/VistaEstadisticasTabController.java
@@ -5,20 +5,37 @@
  */
 package view;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfWriter;
 import controller.Inventario;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javax.imageio.ImageIO;
 import model.Producto;
 
 /**
@@ -40,6 +57,9 @@ public class VistaEstadisticasTabController implements Initializable {
 
     @FXML
     private NumberAxis ejeY;
+    
+     @FXML
+    private Button btnPdf;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -115,5 +135,42 @@ public class VistaEstadisticasTabController implements Initializable {
 
         // añadimos los datos a la gráfica
         grafica.getData().add(serieDatos);
+    }
+    @FXML
+    void btnPdf_MouseClicked(MouseEvent event) {
+        try {
+            //Generamos una imagen a partir de la grafica
+            File file;
+            String pathImagenGrafica = "./grafica_productos_stock.png";
+            String pathPDF = "./grafica_productos_stock.pdf";
+
+            // I. GENERAMOS UN FICHERO DE IMAGEN a partir de la gráfica
+            // --------------------------------------------------------
+            WritableImage image = grafica.snapshot(new SnapshotParameters(), null);
+            file = new File(pathImagenGrafica);
+            ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+
+            //Generamos un pdf a partir de la imagen creada anteriormente
+            Document document = new Document();
+            PdfWriter.getInstance(document, new FileOutputStream(new File(pathPDF)));
+            document.open();
+
+            Image imagenCodigoBarras = Image.getInstance(new File(pathImagenGrafica).getAbsolutePath());
+            imagenCodigoBarras.setAbsolutePosition(10, 500);
+            imagenCodigoBarras.setBorderWidth(0);
+            imagenCodigoBarras.scaleAbsolute(500, 200);
+            document.add(imagenCodigoBarras);
+            document.close();
+
+            //Abrimos el pdf que se acaba de generar
+            if (Desktop.isDesktopSupported()) {
+                file = new File(pathPDF);
+                Desktop.getDesktop().open(file);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(VistaEstadisticasTabController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (DocumentException ex) {
+            Logger.getLogger(VistaEstadisticasTabController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
